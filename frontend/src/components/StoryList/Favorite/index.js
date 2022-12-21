b2a0683519f58873937c43ae635e2206d9ee2b92
tranthangduc <tranthangduc0603@gmail.com>
2022-12-21 21:47:08 +0700

    fix loi tu vung

--- frontend/src/components/StoryList/Favorite/index.js
+++ frontend/src/components/StoryList/Favorite/index.js
@@ -13,9 +13,9 @@ const STORY = {
   description:
     `<div>
       One day a fox spied <Word id={60cfe0c9f5dc801af8279f34} word={code} /> a beautiful <Word id={60d0205de09a5f00072cc4b3} word={expect} />  
-      bunch of ripe <Word id={60d06886f38ce90015e8ba36} word={name} /> grapes hanging <Word id={60d2947ca5b10d0015f98dc4} word={indicate} /> from a vine along the <Word id={60d29a3f012562001586f4f4} word={assure} /> branches of a tree. 
+       bunch of ripe <Word id={60d06886f38ce90015e8ba36} word={name} /> grapes hanging <Word id={60d2947ca5b10d0015f98dc4} word={indicate} /> from a vine along the <Word id={60d29a3f012562001586f4f4} word={assure} /> branches of a tree. 
       The grapes seemed <Word id={60d29be9012562001586f4f9} word={launch} /> ready to burst <Word id={60d2e5de012562001586f506} word={show} /> with juice, 
-      and the fox's <Word id={60d2eab0501c6f0015e40aac} word={implement} /> mouth watered as he <Word id={60d2ebf6501c6f0015e40aaf} word={prvent} /> gazed longingly at them.
+      and the fox's <Word id={60d2eab0501c6f0015e40aac} word={implement} /> mouth watered as he <Word id={60d2ebf6501c6f0015e40aaf} word={prevent} /> gazed longingly at them.
     </div>`,
   picture:
     'https://ila.edu.vn/uploads/SEO/ila-truyen-tieng-anh-cho-be-shutterstock_1296413959.jpg',
